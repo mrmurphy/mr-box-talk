21bec257d474db1f91850c8a4972346f9560d28d
Murphy Randle <murphyspublic@gmail.com>
2014-11-10 19:48:42 -0700

    Add basic mr-box without tests

--- public/mr-box/mr-box.js
+++ public/mr-box/mr-box.js
@@ -1,3 +1,25 @@
 (function() {
-
+	angular.module('mr.directives')
+	.directive('mrBox', function() {
+		return {
+			restrict: 'E',
+			templateUrl: '/mr-box/mr-box.html',
+			replace: true,
+			transclude: true,
+			scope: {
+				loadMsg: "@",
+				errorMsg: "@",
+				resolveFn: "&"
+			},
+			controller: function($scope) {
+				$scope.state = 'loading'
+				$scope.resolveFn()
+				.then(function() {
+					$scope.state = 'loaded'
+				}, function() {
+					$scope.state = 'error'
+				})
+			}
+		}
+	})
 })()
