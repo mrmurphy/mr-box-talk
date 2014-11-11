720dfeadbc7cb9a12f8071d4097bfbc1f447818a
Murphy Randle <murphyspublic@gmail.com>
2014-11-10 22:01:04 -0700

    Add tests

--- public/mr-box/mr-box_spec.js
+++ public/mr-box/mr-box_spec.js
@@ -1,5 +1,50 @@
+// Before each:
+// 1. Declare the module
+// 2. Inject $compile and $rootScope
+// In test:
+// 3. Compile string to element on rootScope
+// 4. rootScope.$digest!
+// 5. Make sure it all worked!
+
 describe('A test', function() {
-	it("should pass", function() {
-		expect(true).toBe(true)
+	var $compile, $rootScope, $q
+
+	beforeEach(module('mr.directives'))
+	beforeEach(module('templates'))
+
+	beforeEach(inject(function(_$compile_, _$rootScope_, _$q_) {
+		$compile = _$compile_
+		$rootScope = _$rootScope_
+		$q = _$q_
+	}))
+
+	it("should render loading text when promise unresolved", function() {
+		$rootScope.fooFn = function() {
+			return $q.defer().promise
+		}
+		var elem = $compile("<mr-box load-msg='foo' " +
+			"resolve-fn='fooFn()'></mr-box>")($rootScope)
+		$rootScope.$digest()
+		expect(elem.html()).toContain("foo")
+	});
+
+	it("should render error text when promise rejected", function() {
+		$rootScope.fooFn = function() {
+			return $q.reject()
+		}
+		var elem = $compile("<mr-box error-msg='ohno' " +
+			"resolve-fn='fooFn()'></mr-box>")($rootScope)
+		$rootScope.$digest()
+		expect(elem.html()).toContain("ohno")
+	});
+
+	it("show transcluded data when promise is resolved", function() {
+		$rootScope.fooFn = function() {
+			return $q.when(true)
+		}
+		var elem = $compile("<mr-box " +
+			"resolve-fn='fooFn()'>Hello, there!</mr-box>")($rootScope)
+		$rootScope.$digest()
+		expect(elem.html()).toContain("Hello, there!")
 	});
 });
